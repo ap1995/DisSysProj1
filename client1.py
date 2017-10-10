24dc9d8a08bf3eae5ab273937d8b550f730a36cc
pvkadam <poojakadam8396@gmail.com>
2017-10-09 21:36:40 -0700

    Create client1.py

---
+++ client1.py
@@ -0,0 +1,27 @@
+from socket import *
+from multiprocessing import Queue
+#from Queue import PriorityQueue
+
+class Client:
+    s = socket()
+    def _init__ (self):
+        sethostname('Ash')
+        numofLikes = 0
+        host = gethostname()
+        port = 12345
+        Queue.PriorityQueue
+
+    def connection(self):
+        s.connect((host, port))
+
+    def send(self):
+        s.listen(5)
+        while True:
+            c, addr = s.accept()
+            print('Got connection from')
+            print(addr)
+            c.sendall('Thank you for connecting'.encode())
+            c.close()
+
+    print(s.recv(1024))
+    s.close()
