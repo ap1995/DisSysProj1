18025f1518921492ad84e45a480134f6ed315259
Ashwini <ap1995@users.noreply.github.com>
2017-10-10 20:33:15 -0700

    Update MainProg.py

--- MainProg.py
+++ MainProg.py
@@ -5,15 +5,5 @@ c2 = config.client2
 c3 = config.client3
 c4 = config.client4
 
-# client1 = client.Client()
-# client1.hostname = 'Ash'
-# client1.port = 12345
-# client1.processID =1
-#
-# client2 = client.Client()
-# client2.hostname = 'Pooja'
-# client2.port = 56789
-# client2.processID =2
-
 c1.connection(c2)
 # c2.connection(c1)
