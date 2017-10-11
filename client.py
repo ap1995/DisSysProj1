3427cb8f6db2f39a358ad273dbcde2e5e8a40a2e
Ashwini <ap1995@users.noreply.github.com>
2017-10-10 20:30:56 -0700

    Update client.py
    Updated Client class functions

--- client.py
+++ client.py
@@ -4,28 +4,73 @@
 # @author: ashwini
 
 from socket import *
-from Queue import PriorityQueue
+# from functions import *
+import sys
+import errno
+import lamportclock
+# from Queue import PriorityQueue
+from multiprocessing import Queue
 
+s = socket()
 class Client:
-    s = socket()
-    def _init_ self:
-        # sethostname('Ash')
+    hostname = ''
+    port = 0
+    def _init_ (self, hostname, port, processID):
+        self.hostname=hostname
+        sethostname(hostname)
         numofLikes = 0
-        host = gethostname()
-        port = 12345
-        # queue.PriorityQueue
+        # self.hostname = gethostname()
+        self.port = port
+        self.processID = processID
+        q = Queue.PriorityQueue
 
-    def connection:
-        s.connect((host, port))
+    def connection(self, other):
+        try:
+            self.socket=socket()
+            self.socket.bind((self.hostname, self.port))
+            # self.socket.create_connection((other.hostname, other.port))
+            self.socket.connect((other.hostname, other.port))
+            self.socket.listen(5)
+            while True:
+                c, addr = s.accept()
+                print('Got connection from')
+                print(addr)
+                c.send('Thank you for connecting'.encode())
+                c.close()
+        except(gaierror):
+            print('There was an error resolving the host')
+            sys.exit()
 
-    def send:
+    def sendLike(self, numofLikes, currentLikes):
+        self.numofLikes = self.numofLikes + currentLikes
+
+    def receive(self):
         s.listen(5)
         while True:
             c, addr = s.accept()
             print('Got connection from')
             print(addr)
-            c.sendall('Thank you for connecting'.encode())
+            c.send('Thank you for connecting'.encode())
+            c.send('Num of Likes'+numofLikes)
             c.close()
+    # def readLike(self):
+        # data
 
-    print(s.recv(1024))
+    def printValue(self):
+        print(s.recv(1024))
+
     s.close()
+
+"""class MyPriorityQueue(PriorityQueue):
+    def __init__(self):
+        PriorityQueue.__init__(self)
+        self.counter = 0
+
+    def put(self, item, priority):
+        PriorityQueue.put(self, (priority, self.counter, item))
+        self.counter += 1
+
+    def get(self, *args, **kwargs):
+        _, _, item = PriorityQueue.get(self, *args, **kwargs)
+        return item
+"""
