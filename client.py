a3e2f352e1c546bbdf3cc6b16cacb124e883cf09
Ashwini <ap1995@users.noreply.github.com>
2017-10-12 18:46:11 -0700

    Update client.py
    Updated server and client functions

--- client.py
+++ client.py
@@ -4,62 +4,103 @@
 # @author: ashwini
 
 from socket import *
-# from functions import *
+# from thread import *
 import sys
-import errno
 import lamportclock
 # from Queue import PriorityQueue
 from multiprocessing import Queue
 
-s = socket()
 class Client:
     hostname = ''
     port = 0
-    def _init_ (self, hostname, port, processID):
-        self.hostname=hostname
-        sethostname(hostname)
-        numofLikes = 0
-        # self.hostname = gethostname()
-        self.port = port
-        self.processID = processID
-        q = Queue.PriorityQueue
+    client_list = []
+    s = None
 
-    def connection(self, other):
+# Initializing Client and its attributes
+    def __init__ (self, port, processID):
+        self.numofLikes = 0
+        self.port = port
+        self.hostname = gethostname()
+        self.processID = processID
+        #q = Queue.PriorityQueue
+        self.s = socket(AF_INET, SOCK_STREAM)
+
+# To start the server part of the Client
+    def startServer(self): ### Something is missing here
         try:
-            self.socket=socket()
-            self.socket.bind((self.hostname, self.port))
-            # self.socket.create_connection((other.hostname, other.port))
-            self.socket.connect((other.hostname, other.port))
-            self.socket.listen(5)
+            self.s.bind((self.hostname, self.port))
+            self.s.listen(5)
+            print("server started on port " + str(self.port))
             while True:
-                c, addr = s.accept()
+                c, addr = self.s.accept()
                 print('Got connection from')
                 print(addr)
-                c.send('Thank you for connecting'.encode())
-                c.close()
+                # c.send(b'Thank you for connecting')
+                #self.client_list.append(c)
+                # print(c.recv(1024))
+                # c.close()
         except(gaierror):
-            print('There was an error resolving the host')
+            print('There was an error connecting to the host')
             sys.exit()
+            self.s.close()
 
-    def sendLike(self, numofLikes, currentLikes):
-        self.numofLikes = self.numofLikes + currentLikes
+# To connect to the other client/server
+    def startClient(self, port):
+        try:
+            cSocket=socket(AF_INET, SOCK_STREAM)
+            # self.socket.create_connection((other.hostname, other.port))
+            cSocket.connect((gethostname(), port))
+            while True:
+                print('Enter 1 to like, 0 to exit:')
+                message = sys.stdin.readline().strip()
+                if (message == 1):
+                    self.numofLikes = self.numofLikes+1;
+                if (message == 0):
+                    cSocket.close()
+                if((message != 1) and (message != 0))
+                    print('Invalid Input')
+                cSocket.send(self.numofLikes)
+        except:
+            cSocket.close()
 
-    def receive(self):
-        s.listen(5)
-        while True:
-            c, addr = s.accept()
-            print('Got connection from')
-            print(addr)
-            c.send('Thank you for connecting'.encode())
-            c.send('Num of Likes'+numofLikes)
-            c.close()
-    # def readLike(self):
-        # data
 
-    def printValue(self):
-        print(s.recv(1024))
 
-    s.close()
+    def sendToAll(self, message):
+        for client in client_list:
+            try:
+                client.send(message)
+            except:
+                client.close()
+
+    def closeSocket(self):
+        self.s.close()
+
+
+# # Waits for input from user to add likes
+#     def runCLient(self):
+#         m.getch()
+#
+# # To add likes to the post
+#     def addLike(self, numofLikes, currentLikes):
+#         self.numofLikes = self.numofLikes + currentLikes
+#
+# # To receive info from other client/servers when they update the likes on the post
+#     # def receive(self):
+#     #     s.listen(5)
+#     #     while True:
+#     #         c, addr = s.accept()
+#     #         print('Got connection from')
+#     #         print(addr)
+#     #         c.send('Thank you for connecting'.encode())
+#     #         c.send('Num of Likes'+numofLikes)
+#     #         c.close()
+#     # def readLike(self):
+#         # data
+#
+#     def printValue(self):
+#         print(s.recv(1024))
+#
+#     s.close()
 
 """class MyPriorityQueue(PriorityQueue):
     def __init__(self):
