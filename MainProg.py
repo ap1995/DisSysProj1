448a4a8a6d3f642b4928cbfbd7224ff88d7653f7
Ashwini <ap1995@users.noreply.github.com>
2017-10-12 18:48:25 -0700

    Update MainProg.py

--- MainProg.py
+++ MainProg.py
@@ -5,5 +5,8 @@ c2 = config.client2
 c3 = config.client3
 c4 = config.client4
 
-c1.connection(c2)
-# c2.connection(c1)
+# Make all connections, will be added to config file later
+c1.startServer
+c2.startServer
+c1.startClient(c2.port)
+c2.startClient(c1.port)
