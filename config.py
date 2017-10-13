93bbf720759d8f987a05cc56b66c028508e0a214
Ashwini <ap1995@users.noreply.github.com>
2017-10-12 18:47:18 -0700

    Update config.py
    Updated clients

--- config.py
+++ config.py
@@ -1,22 +1,10 @@
 # Config file
 import client
 
-client1 = client.Client()
-client1.name = 'Ash'
-client1.port = 12345
-client1.processID =1
+client1 = client.Client(12345, 1)
 
-client2 = client.Client()
-client2.name = 'Pooja'
-client2.port = 56789
-client2.processID =2
+client2 = client.Client(13456, 2)
 
-client3 = client.Client()
-client3.name = 'Rahul'
-client3.port = 17834
-client3.processID =3
+client3 = client.Client(14834, 3)
 
-client4 = client.Client()
-client4.name = 'Rohit'
-client4.port = 98765
-client4.processID = 4
+client4 = client.Client(15685, 4)
