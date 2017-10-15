ebacaf02e61f136fc912a6d3a2916df37d67103d
Ashwini <ap1995@users.noreply.github.com>
2017-10-15 10:46:35 -0700

    Update client.py
    Working connections

--- client.py
+++ client.py
@@ -6,137 +6,103 @@
 from socket import *
 from _thread import *
 import json
-import socialMedia
+import socialmedia
 import threading
+# from multiprocessing import Process
 import time
 import sys
-import lamportclock
-# from Queue import PriorityQueue
-from multiprocessing import Queue
-
-
-# def awaitInput(self, conn):
-#     try:
-#         while True:
-#             print(conn.recv(1024))
-#     except:
-#         print('Exception in awaitInput function')
+# import lamportclock
 
 # To connect to the other client/server
-
-with open('config.json') as configfile:
-    configdata = json.load(configfile)
 
 class Client:
     hostname = ''
     port = 0
-    client_list = []
     s = None
-    socialmedia = socialMedia()
 
 # Initializing Client and its attributes
     def __init__ (self, ID):
         self.numofLikes = 0
-        port = configdata["systems"][ID]
+        print("System running: "+ ID)
+        port = configdata["systems"][ID][1]
         self.port = port
         self.hostname = gethostname()
         #q = Queue.PriorityQueue
         self.s = socket(AF_INET, SOCK_STREAM)
-        self.startClient(configdata)
-        # self.awaitInput()
-        
-        # self.startListening()
+        # self.connectToAll()
 
-    def startClient(self, configdata):
-        # try:
-            cSocket = socket(AF_INET, SOCK_STREAM)
-            # self.socket.create_connection((other.hostname, other.port))
-            for i in configdata["systems"]:
-                cSocket.connect((gethostname(), configdata["systems"][i]))
+        start_new_thread(self.startListening, ())
+        start_new_thread(self.awaitInput, ())
+        # time.sleep(configdata["delay"])
+        while True:
+            pass
 
-
-                    # cSocket.send(self.numofLikes)
-        # except:
-        #     print('exception')
-        #     cSocket.close()
-
+    def receiveMessages(self, conn, addr):
+        # while True:
+            print('Message received from Client at port '+ str(addr[1]))
+            print(conn.recv(1024).decode())
 
     def awaitInput(self):
+        # time.sleep(delay)
         while True:
-            # dummy = sys.stdin.readline().strip()
-            message = input('Enter 1 to like:')
+            # print(self.s.recv(1024))
+            message = input('Enter 1 to like: ')
             message = int(message)
             if (message == 1):
-                socialmedia.numofLikes = socialmedia.numofLikes + 1  # start thread to send current likes, with socialmedia object, to everyone and then kill
-                self.numofLikes = socialmedia.numofLikes
-                self.sendToAll(socialmedia.string1 + "Total number of likes "+ socialmedia.numofLikes)
-                # cSocket.send(b'numofLikes' + b'str(socialmedia.numofLikes)')
-            # if (message == 0):
-            #     cSocket.close()
+                sm.numofLikes = sm.numofLikes + 1  # start thread to send current likes, with socialmedia object, to everyone and then kill
+                self.numofLikes = sm.numofLikes
+                tosend = "Post: \n" + str(sm.string1) + "\nCurrent like count " + str(sm.numofLikes)
+                print(tosend)
+                self.sendToAll(tosend)
+            # else:
+            #     print('Invalid input')
+                # self.sendToAll(sm.string1 + "Total number of likes "+ str(sm.numofLikes))
+                # cSocket.send(b'numofLikes' + b'str(socialmedia.numofLikes)'
             # if ((message != 1) and (message != 0)):
             #     print('Invalid Input')
 
 # To start the server part of the Client
-    def startListening(self): ### Threading part is incomplete here
+    def startListening(self):
         try:
-            self.s.bind((self.hostname, self.port))
+            self.s.bind((self.hostname, int(self.port)))
             self.s.listen(4)
             print("server started on port " + str(self.port))
             while True:
                 c, addr = self.s.accept()
+                conn = c
                 print('Got connection from')
                 print(addr)
-                start_new_thread(self.startClient, (configdata,)) # conneciton dictionary
+                start_new_thread(self.receiveMessages, (conn, addr)) # connection dictionary
                 # c.close()
         except(gaierror):
             print('There was an error connecting to the host')
-            sys.exit()
             self.s.close()
+            sys.exit()
 
+# To send updated likes to everyone
     def sendToAll(self, message):
         for i in configdata["systems"]:
-            self.s.send(message)
+            if (configdata["systems"][i][1] == self.port):
+                continue
+            else:
+                cSocket = socket(AF_INET, SOCK_STREAM)
+                ip, port = configdata["systems"][i]
+                port = int(port)
+                cSocket.connect((gethostname(), port))
+                print('Connected to port number ' + configdata["systems"][i][1])
+                cSocket.send(message.encode())
+                print('Message sent to Client at port '+ str(port))
+                cSocket.close()
 
     def closeSocket(self):
         self.s.close()
 
+######## MAIN #########
 
-# # Waits for input from user to add likes
-#     def runCLient(self):
-#         m.getch()
+with open('config.json') as configfile:
+    configdata = json.load(configfile)
 
-# # To add likes to the post
-#     def addLike(self, numofLikes, currentLikes):
-#         self.numofLikes = self.numofLikes + currentLikes
-
-# To receive info from other client/servers when they update the likes on the post
-    # def receive(self):
-    #     s.listen(5)
-    #     while True:
-    #         c, addr = s.accept()
-    #         print('Got connection from')
-    #         print(addr)
-    #         c.send('Thank you for connecting'.encode())
-    #         c.send('Num of Likes'+numofLikes)
-    #         c.close()
-    # def readLike(self):
-        # data
-#
-#     def printValue(self):
-#         print(s.recv(1024))
-#
-#     s.close()
-
-# class MyPriorityQueue(PriorityQueue):
-#     def __init__(self):
-#         PriorityQueue.__init__(self)
-#         self.counter = 0
-#
-#     def put(self, item, priority):
-#         PriorityQueue.put(self, (priority, self.counter, item))
-#         self.counter += 1
-#
-#     def get(self, *args, **kwargs):
-#         _, _, item = PriorityQueue.get(self, *args, **kwargs)
-#         return item
-
+sm = socialmedia.Socialmedia
+delay = configdata["delay"]
+ID = sys.argv[1]
+c = Client(ID)
