d9c7f4ede19305b098af63969960738cb9bb0383
Ashwini <ap1995@users.noreply.github.com>
2017-10-15 18:53:58 -0700

    Update lamportclock.py
    Update till 6:53PM

--- lamportclock.py
+++ lamportclock.py
@@ -1,42 +1,36 @@
 import threading
 import time
-
+from heapq import *
 from multiprocessing import Queue
-
-
+from queue import PriorityQueue
 
 class LamportClock:
-    def _init_ (self, processID, queue):
+    time = 0
+    processID = 0
+    def __init__(self, time, processID, queue):
         self.lock = threading.Lock()
         self.time = 0
-        # self.processID = processID
-        # self.queue = queue
-
-    def addtoRequestQueue(self, queue, item):
-        queue.put(item)
-        # self.queue.put(("S" + str(self.processID)), self.processID)
-
-    def removefromRequestQ(self, queue):
-        return queue.get()
-
-    def printRequestQ(self, queue):
-        q1= queue
-        while q1.qsize() != 0:
-            print(q1.get())
+        self.processID = processID
+        self.queue = queue
 
     def incrementTime(self):
-        self.lock.acquire()
-        try:
-            self.time = self.time +1
-        finally:
-            self.lock.release()
+        # self.lock.acquire()
+        # try:
+        self.time = self.time +1
+        # finally:
+        #     self.lock.release()
+        print("Current Lamport time is "+ self.getLamportTime())
+        # print("Current Lamport time "+ str(self.time)+"."+str(self.processID))
 
     def getLocalTime(self):
-        self.lock.acquire()
-        try:
-            return self.time
-        finally:
-            self.lock.release()
+        return self.time
+
+    def getLamportTime(self):
+        # self.lock.acquire()
+        # try:
+        return (str(self.time) + "."+ str(self.processID))
+        # finally:
+        #     self.lock.release()
 
     """def getTotallyOrderedTime(self):
         self.lock.acquire()
@@ -46,13 +40,10 @@ class LamportClock:
             self.lock.release()
             """
 
-    # def addToQueue(self, port):
-    #     PriorityQueue.put(port, int(port)-4000)
-
     def compareTime(self, other):
-        self.lock.acquire()
-        try:
-            self.time = max(self.time, other) +1
-            return self.time
-        finally:
-            self.lock.release()
+        # self.lock.acquire()
+        # try:
+        self.time = max(self.time, other) +1
+        return self.time
+        # finally:
+            # self.lock.release()
